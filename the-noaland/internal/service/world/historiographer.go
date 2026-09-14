d694ef406509b44db4582ffd61a5175933fc37a3
Edward NoaLand <edwardnoaland@gmail.com>
2026-09-14 14:26:32 +0100

    feat(historiographer): introduce historiographer in the world

---
+++ the-noaland/internal/service/world/historiographer.go
@@ -0,0 +1,78 @@
+package world
+
+import (
+	"fmt"
+
+	"github.com/NoaLand/the-noaland/the-noaland/internal/service/world/entity"
+	"github.com/NoaLand/the-noaland/the-noaland/internal/service/world/entity/worldtree"
+)
+
+type record struct {
+	time uint64
+	entityId entity.EntityID
+	message string
+}
+
+type annal struct {
+	records []record
+}
+
+type historiographer struct{
+	last map[entity.EntityID]any
+	annal annal
+}
+
+func NewHistoriographer() *historiographer {
+	return &historiographer{
+		last: make(map[entity.EntityID]any),
+	}
+}
+
+func (h *historiographer) record(time uint64, entity entity.Entity) {
+	switch v := entity.(type) {
+	case *worldtree.WorldTree:
+		h.recordWorldTree(time, v)
+	}
+}
+
+func (h *historiographer) recordWorldTree(time uint64, tree *worldtree.WorldTree) {
+	current := tree.Express()
+
+	previous, ok := h.last[tree.ID()]
+	if !ok {
+		h.last[tree.ID()] = current
+		return
+	}
+
+	previousExpression, ok := previous.(*worldtree.Expressions)
+	if !ok {
+		return
+	}
+
+	if previousExpression.Appearance != current.Appearance {
+		record := record{
+			time:     time,
+			entityId: tree.ID(),
+			message:  worldTreeMessage(previousExpression, current),
+		}
+
+		h.annal.records = append(h.annal.records, record)
+
+		fmt.Printf(
+			"Time #%d - %s: %s\n",
+			record.time,
+			record.entityId,
+			record.message,
+		)
+	}
+
+	h.last[tree.ID()] = current
+}
+
+func worldTreeMessage(previous, current *worldtree.Expressions) string {
+	return fmt.Sprintf(
+		"The World Tree changed from %s to %s.",
+		previous.Appearance,
+		current.Appearance,
+	)
+}
--- the-noaland/internal/service/world/world.go
+++ the-noaland/internal/service/world/world.go
@@ -5,23 +5,31 @@ import (
 )
 
 type World struct {
-	entities []entity.Entity
-	seed     uint64
+	entities        []entity.Entity
+	seed            uint64
+	historiographer *historiographer
+	timer           uint64
 }
 
 func New(seed uint64) *World {
 	return &World{
-		seed: seed,
+		seed:            seed,
+		historiographer: NewHistoriographer(),
+		timer: 0,
 	}
 }
 
 func (world *World) Add(entity entity.Entity) {
 	world.entities = append(world.entities, entity)
+	world.historiographer.record(world.timer, entity)
 }
 
 func (world *World) Step() {
+	world.timer++
+
 	for _, entity := range world.entities {
 		entity.Step()
+		world.historiographer.record(world.timer, entity)
 	}
 }
 
